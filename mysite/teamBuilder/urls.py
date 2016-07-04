4ecff8bd86f445dd1a6748d2a592451138f8ae54
williamking <williamjwking@gmail.com>
2016-07-04 16:54:01 +0800

    add route to view pages

--- mysite/teamBuilder/urls.py
+++ mysite/teamBuilder/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 app_name = 'teamBuilder'
 urlpatterns = [
+    url(r'^user/(?P<id>[0-9]+)', views.UserProfileView.as_view()),
+    url(r'^team/(?P<id>[0-9]+)', views.TeamDetailView.as_view()),
+    url(r'^project/create$', views.ProjectCreateView.as_view()),
+    url(r'^project/createTeam/(?P<id>[0-9]+)', views.TeamCreateView.as_view()),
+    url(r'^project/manage/(?P<id>[0-9]+)', views.ProjectManageView.as_view()), 
+    url(r'^project/(?P<id>[0-9]+)', views.ProjectDetailView.as_view())
 ]
--- mysite/teamBuilder/views.py
+++ mysite/teamBuilder/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
+from django.views.generic import TemplateView
 
 from django.contrib.auth.models import User
 from .models import *
@@ -8,3 +9,20 @@ from .forms import *
 
 # Create your views here.
 
+class UserProfileView(TemplateView):
+	template_name = 'accounts/profile.html'
+
+class TeamDetailView(TemplateView):
+	template_name = 'team/team_detail.html'
+
+class ProjectCreateView(TemplateView):
+	template_name = 'project/create_project.html'
+
+class TeamCreateView(TemplateView):
+	template_name = 'team/create_team.html'
+
+class ProjectManageView(TemplateView):
+	template_name = 'project/manage_project.html'
+
+class ProjectDetailView(TemplateView):
+	template_name = 'project/project_detail.html'
